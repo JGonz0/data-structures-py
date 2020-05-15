e109ad0f443e13327280b49dcce9e5edda64c7af
unknown <josgonzalopg@gmail.com>
2020-05-15 05:27:32 -0600

    Linked list and stack structures without comments

---
+++ linkedlist.py
@@ -0,0 +1,123 @@
+
+class LinkedNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    
+    def get_node(self):
+        return self.data
+    
+    def set_node(self,data):
+        self.data = data
+        return
+    
+    def get_next(self):
+        return self.next
+    
+    def set_next(self, node):
+        self.next = node
+        return
+    
+class LinkedList:
+    def __init__(self,head=None):
+        self.head = head
+
+    def add(self,data):
+        new_node = LinkedNode(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        
+        current_node = self.head
+        while current_node.next is not None:
+            current_node = current_node.next
+        current_node.next = new_node
+        return
+    
+    def node_add(self,node):
+        if type(node) is type(LinkedNode(1)):
+            if self.head is None:
+                self.head = node
+                return
+            
+            current_node = self.head
+            while True:
+                if current_node.next is None:
+                    current_node.next = node
+                    return
+                current_node = current_node.next
+        else:
+            print("Operacion invalida")
+    
+    def display(self):
+        current_node = self.head
+        while current_node is not None:
+            print("%d -> " % current_node.data, end='')
+            current_node = current_node.next
+        print("None")
+        return
+    
+    def len(self):
+        current_node = self.head
+        leng = 0
+        while True:
+            if current_node is None:
+                return leng
+            current_node = current_node.next
+            leng += 1
+    
+    def insertion(self,data,idx):
+        if idx > self.len():
+            print("Indice fuera de limites")
+            return
+        
+        new_node = LinkedNode(data)
+        if idx == 0:
+            aux_node = self.head
+            self.head = new_node
+            self.head.next = aux_node
+            return
+        
+        current_node = self.head
+        current_idx = 0
+        while True:
+            if current_idx == idx - 1:
+                aux_node = current_node.next
+                current_node.next = new_node
+                new_node.next = aux_node
+                return
+            current_node = current_node.next
+            current_idx += 1
+    
+    def remover(self,idx):
+        if idx > self.len() - 1:
+            print("Indice fuera de limites")
+            return
+        
+        if idx == 0:
+            self.head = self.head.next
+            return
+        
+        current_node = self.head
+        current_idx = 0
+        while True:
+            if current_idx == idx-1:
+                current_node.next = current_node.next.next
+                return
+            current_node = current_node.next
+            current_idx += 1
+
+if __name__ == '__main__':
+    
+    link = LinkedList()
+    n1 = LinkedNode(5)
+    n2 = LinkedNode(49)
+    n3 = LinkedNode(30)
+
+    n1.next = n2
+    n2.next = n3
+
+    n3.set_next(LinkedNode(20))
+
+    link.node_add(n1)
+    link.display()
---
+++ stack.py
@@ -0,0 +1,94 @@
+class UniqueStack:
+
+    def __init__(self):
+        self._stack = []
+        self._set = set()
+    
+    def push(self,data):
+        if data not in self._set:
+            self._stack.append(data)
+            self._set.add(data)
+            return
+        print("Error: Duplicated data in the stack")
+        return
+    
+    def pop(self):
+        if self._stack == []:
+            print("Stack is empty")
+            return
+        return self._stack.pop()
+
+    def graphic_print(self):
+        rev_stack = self._stack[::-1]
+        for i in range(len(rev_stack)):
+            if i == 0:
+                print("[ Last Item ]")
+            print("-> %s" %rev_stack[i])
+            if i == len(rev_stack) - 1:
+                print("[ First Item ]")
+        return
+
+    def peek(self):
+        return self._stack[-1]
+
+    def show(self):
+        print(self._stack)
+        return
+
+class Stack:
+    def __init__(self):
+        self._stack = []
+    
+    def push(self,data):
+        self._stack.append(data)
+        return
+    
+    def pop(self):
+        if self._stack == []:
+            print("Stack is empty")
+            return
+        return self._stack.pop()
+    
+    def peek(self):
+        return self._stack[-1]
+    
+    def graphic_print(self):
+        rev_stack = self._stack[::-1]
+        for i in range(len(rev_stack)):
+            if i == 0:
+                print("[ Last Item ]")
+            print("-> %s" %rev_stack[i])
+            if i == len(rev_stack) - 1:
+                print("[ First Item ]")
+        return
+    
+    def show(self):
+        print(self._stack)
+        return
+    
+    def replicate(self, times):
+        if times < 0:
+            print("Invalid number of times")
+            return
+        dup = self.pop()
+        for _ in range(times):
+            self.push(dup)
+        return
+
+if __name__ == '__main__':
+
+    UVstack = UniqueStack()
+    UVstack.push(10)
+    UVstack.push('Foo')
+    UVstack.push('30.45')
+    UVstack.pop()
+    UVstack.graphic_print()
+
+    print("\n")
+
+    Astack = Stack()
+    Astack.push(5)
+    Astack.push(5)
+    Astack.push(7)
+    Astack.replicate(3)
+    Astack.graphic_print()
